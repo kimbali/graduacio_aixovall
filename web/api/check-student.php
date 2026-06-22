1185c29b993f0b2ddca30ab9c9b4b04828fda01b
kimbali <38001986+kimbali@users.noreply.github.com>
2026-06-22 11:50:50 +0200

    Adapt structure for cdmon web directory

--- web/api/check-student.php
+++ web/api/check-student.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types=1);
-require_once __DIR__ . '/../secrets/db.php';
+require_once __DIR__ . '/../../secrets/db.php';
 
 $data = readJsonBody();
 $nia = normalizeNia($data['nia'] ?? '');
